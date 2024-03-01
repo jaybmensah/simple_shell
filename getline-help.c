95c99d6bb3789981e1cf0ec37ff6bb7a969a9593
jaybmensah <lioneljbmensah@gmail.com>
2024-03-01 00:26:57 +0000

    getline-help modifications

--- getline-help.c
+++ getline-help.c
@@ -21,25 +21,37 @@ int needs_realloc(size_t cur_syz, size_t cur_len, ssize_t bytes_read)
  *
  * Return: 0 on success, -1 on failure
  */
-int concatenate_chunk(char **line, size_t line_size, ssize_t bytes_read)
+ssize_t concatenate_chunk(FILE *fp, char **line, size_t line_size, ssize_t bytes_read)
 {
-	size_t current_length = cust_strlen(*line);
-	char chunk[1024];   /* Assuming a reasonable size for the chunk
-*/
-	if (current_length + bytes_read >= line_size)
+	ssize_t chunk_bytes;
+	char chunk[1024]; // Assuming a reasonable size for the chunk
+
+	if ((*line) == NULL)
 	{
-		return (-1);   /* Error: Buffer overflow */
+		(*line) = malloc(line_size);
+		if ((*line) == NULL)
+			return (-1);
+	}
+	chunk_bytes = fread(chunk, 1, bytes_read, fp);
+
+	if (line_size < (size_t)bytes_read)
+	{
+		free(*line);
+		*line = NULL;
+		return (-1);
 	}
 
-	ssize_t chunk_bytes = read_from_file(fp, chunk, sizeof(chunk));
+	chunk_bytes = read_from_file(fp, chunk, sizeof(chunk));
 
 	if (chunk_bytes == -1)
 	{
-		return (-1);   /* Error reading from file */
+		free(*line);
+		*line = NULL;
+		return (-1);
 	}
 
 	strncat(*line, chunk, chunk_bytes);
-	return (0);   /* Success */
+	return chunk_bytes;
 }
 
 /**
